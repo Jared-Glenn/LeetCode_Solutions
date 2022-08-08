f383735351331b2846b0886c301229e238ee53fe
Jared <38020231+Jared-Glenn@users.noreply.github.com>
2022-08-07 23:45:43 -0600

    0.36
    155. Min Stack

---
+++ LeetCode Solutions/Medium Level/155_Min_Stack.py
@@ -0,0 +1,92 @@
+'''
+155. Min Stack
+
+Medium
+
+9061
+
+656
+
+Add to List
+
+Share
+Design a stack that supports push, pop, top, and retrieving the minimum element in constant time.
+
+Implement the MinStack class:
+
+MinStack() initializes the stack object.
+void push(int val) pushes the element val onto the stack.
+void pop() removes the element on the top of the stack.
+int top() gets the top element of the stack.
+int getMin() retrieves the minimum element in the stack.
+You must implement a solution with O(1) time complexity for each function.
+
+ 
+
+Example 1:
+
+Input
+["MinStack","push","push","push","getMin","pop","top","getMin"]
+[[],[-2],[0],[-3],[],[],[],[]]
+
+Output
+[null,null,null,null,-3,null,0,-2]
+
+Explanation
+MinStack minStack = new MinStack();
+minStack.push(-2);
+minStack.push(0);
+minStack.push(-3);
+minStack.getMin(); // return -3
+minStack.pop();
+minStack.top();    // return 0
+minStack.getMin(); // return -2
+ 
+
+Constraints:
+
+-231 <= val <= 231 - 1
+Methods pop, top and getMin operations will always be called on non-empty stacks.
+At most 3 * 104 calls will be made to push, pop, top, and getMin.
+
+'''
+class MinStack:
+
+    def __init__(self):
+        self.stack = []
+        self.minstack = []
+
+    def push(self, val: int) -> None:
+        self.stack.append(val)
+        if self.minstack:
+            val = min(val, self.minstack[-1])
+        self.minstack.append(val)
+        
+
+    def pop(self) -> None:
+        self.minstack.pop()
+        return self.stack.pop()
+
+    def top(self) -> int:
+        return self.stack[-1]
+
+    def getMin(self) -> int:
+        return self.minstack[-1]
+        
+
+
+# Your MinStack object will be instantiated and called as such:
+# obj = MinStack()
+# obj.push(val)
+# obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.getMin()
+
+
+
+'''
+Success
+Details 
+Runtime: 76 ms, faster than 78.13% of Python3 online submissions for Min Stack.
+Memory Usage: 18 MB, less than 55.56% of Python3 online submissions for Min Stack.
+'''
