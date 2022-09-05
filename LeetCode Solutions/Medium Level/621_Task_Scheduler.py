cf4f2396f3151a82e32c3ec325e80e9c7a998b72
Jared <38020231+Jared-Glenn@users.noreply.github.com>
2022-09-05 13:15:25 -0600

    0.84
    621. Task Scheduler
    * Completed the code.

--- LeetCode Solutions/Medium Level/621_Task_Scheduler.py
+++ LeetCode Solutions/Medium Level/621_Task_Scheduler.py
@@ -54,10 +54,28 @@ The integer n is in the range [0, 100].
 class Solution:
     def leastInterval(self, tasks: List[str], n: int) -> int:
         
-        
-        maxHeap = heapq.heapify()
+        count = Counter(tasks)
+        maxHeap = [-cnt for cnt in count.values()]
+        heapq.heapify(maxHeap)
         
         time = 0
+        q = deque()
         
-        while maxHeap and queue:
+        while maxHeap or q:
             time += 1
+            
+            if maxHeap:
+                cnt = 1 + heapq.heappop(maxHeap)
+                if cnt:
+                    q.append([cnt, time + n])
+                    
+            if q and q[0][1] == time:
+                heapq.heappush(maxHeap, q.popleft()[0])
+        return time
+    
+'''
+Success
+Details 
+Runtime: 1765 ms, faster than 5.01% of Python3 online submissions for Task Scheduler.
+Memory Usage: 14.4 MB, less than 42.52% of Python3 online submissions for Task Scheduler.
+'''
