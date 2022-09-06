d93d118f7926f72227102b70b829b9b1619aa984
Jared <38020231+Jared-Glenn@users.noreply.github.com>
2022-09-06 14:00:27 -0600

    0.86
    295. Find Median from Data Stream

---
+++ LeetCode Solutions/Hard Level/295_Find_Median_from_Data_Stream.py
@@ -0,0 +1,82 @@
+'''
+295. Find Median from Data Stream
+
+Hard
+
+8109
+
+147
+
+Add to List
+
+Share
+The median is the middle value in an ordered integer list. If the size of the list is even, there is no middle value and the median is the mean of the two middle values.
+
+For example, for arr = [2,3,4], the median is 3.
+For example, for arr = [2,3], the median is (2 + 3) / 2 = 2.5.
+Implement the MedianFinder class:
+
+MedianFinder() initializes the MedianFinder object.
+void addNum(int num) adds the integer num from the data stream to the data structure.
+double findMedian() returns the median of all elements so far. Answers within 10-5 of the actual answer will be accepted.
+ 
+
+Example 1:
+
+Input
+["MedianFinder", "addNum", "addNum", "findMedian", "addNum", "findMedian"]
+[[], [1], [2], [], [3], []]
+Output
+[null, null, null, 1.5, null, 2.0]
+
+Explanation
+MedianFinder medianFinder = new MedianFinder();
+medianFinder.addNum(1);    // arr = [1]
+medianFinder.addNum(2);    // arr = [1, 2]
+medianFinder.findMedian(); // return 1.5 (i.e., (1 + 2) / 2)
+medianFinder.addNum(3);    // arr[1, 2, 3]
+medianFinder.findMedian(); // return 2.0
+ 
+
+Constraints:
+
+-105 <= num <= 105
+There will be at least one element in the data structure before calling findMedian.
+At most 5 * 104 calls will be made to addNum and findMedian.
+ 
+
+Follow up:
+
+If all integer numbers from the stream are in the range [0, 100], how would you optimize your solution?
+If 99% of all integer numbers from the stream are in the range [0, 100], how would you optimize your solution?
+'''
+
+class MedianFinder:
+
+    def __init__(self):
+        self.list = []
+
+    def addNum(self, num: int) -> None:
+        self.list.append(num)
+
+    def findMedian(self) -> float:
+        self.list.sort()
+        length = len(self.list)
+        if length%2 == 0:
+            median = (self.list[length//2] + self.list[(length//2)-1])/2
+        else:
+            median = self.list[(length//2)]
+        return median
+
+
+# Your MedianFinder object will be instantiated and called as such:
+# obj = MedianFinder()
+# obj.addNum(num)
+# param_2 = obj.findMedian()
+
+'''
+Success
+Details 
+Runtime: 6751 ms, faster than 5.00% of Python3 online submissions for Find Median from Data Stream.
+Memory Usage: 35.7 MB, less than 86.93% of Python3 online submissions for Find Median from Data Stream.
+'''
